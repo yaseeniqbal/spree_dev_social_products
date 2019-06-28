aae2f7cc6dab62fa2227421a8d2310f564986be0
yaseen iqbal <p136011@nu.edu.pk>
2019-06-28 22:23:04 +0500

    adjustments

--- app/overrides/add_social_buttons_to_products.rb
+++ app/overrides/add_social_buttons_to_products.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/products/_main_details',
   name: 'add_social_buttons_to_products_show',
-  insert_after: '.social-dv-cth',
+  insert_after: '.vendor-name-sec',
   partial: 'spree/shared/social_buttons'
 )
